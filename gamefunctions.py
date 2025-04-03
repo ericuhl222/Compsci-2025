c1f021627130f172387d6d0a45b698ed8f0fe023
Eric <Uhleric@gmail.com>
2025-04-03 14:44:07 -0600

    uploading current version of the game with shop code

--- gamefunctions.py
+++ gamefunctions.py
@@ -1,21 +1,120 @@
-#Eric Uhl
-#2.17.25
-#computer science
-#i usually dont win
-
-"""This is a module for game.py, it has a purchase item function, name prompt, a monster generator, and it prints a shop menu.
-
-recently replaced print commands with return, everything appears to be functional.
-"""
-
-
-
-""" this is a program for purchasing an item, it takes starting money and subtracts item(s)value from your total.
-
-purchase_item ((random.random()), random.randint(1,6), random.randint(0,4))
-This is how you call the function"""
-
 import random
+
+
+#town choice / what 2 do
+"""Gets the user's choice in town and validates it."""
+def get_user_town_choice(player_hp, gold):
+
+    while True:
+        print("\nYou are in town.")
+        print(f"Current HP: {player_hp}, Current Gold: {gold}")
+        print("What would you like to do?")
+        print("1) Leave town (Fight Monster)")
+        print("2) Sleep (Restore HP for 5 Gold)")
+        print("3) Check your inventory")
+        print("4) Go to the shop")
+        print("5) Quit")
+
+        choice = input("Enter your choice (1, 2, 3, 4, or 5): ")
+        if choice in ("1", "2", "3", "4", "5"):
+            return choice
+        else:
+            print("Invalid choice. Please enter 1, 2, 3, 4, or 5.")
+
+#new random monster code
+     #this will need to be balanced, but could be fun to have OP bad guy
+"""generates one of three monster types to fight later
+ called with new_random_monster
+
+ returns sel_health, sel_money, sel_monster, sel_scenario"""
+
+def new_random_monster():
+    monsters = ["demon", "ghost", "goblin", "creeper"]
+    health = random.randint(0,25)
+    sel_money = random.randint(0,24)
+    power = random.randint(1,3)
+    sel_health = health * power
+    sel_monster = random.choice(monsters)
+    #scenarios:
+    scenario = [(f'You come across a {sel_monster} eating a corpse'), (f"A {sel_monster} jumps out from beind a crate"), (f'After entering a cave a {sel_monster} attacks you from a dark corner')]
+    sel_scenario = random.choice(scenario)
+                                                                   
+    return(sel_health, sel_money, sel_monster, sel_scenario)
+
+#tussling 
+"""Code for handling health and fights within the game
+
+called by using handle_fight(player_hp, gold):
+"""
+def handle_fight(player_hp, gold):
+    monster_hp = 15
+    monster_attack = 5
+    monster_type = "Goblin"
+    print(f"A wild {monster_type} appears!")
+
+    while player_hp > 0 and monster_hp > 0:
+        display_fight_statistics(player_hp, monster_hp)
+        choice = get_user_fight_options()
+
+        if choice == "1":
+            player_attack = random.randint(3, 8)
+            monster_hp -= player_attack
+            print(f"You attack the {monster_type} for {player_attack} damage.")
+
+            if monster_hp > 0:
+                monster_damage = random.randint(1, monster_attack)
+                player_hp -= monster_damage
+                print(f"The {monster_type} attacks you for {monster_damage} damage.")
+
+        elif choice == "2":
+            print("You run away!")
+            return player_hp, gold
+
+    if player_hp <= 0:
+        print("You were defeated!")
+        return 0, gold
+    else:
+        print(f"You defeated the {monster_type}!")
+        gold += 10
+        print("You found 10 gold.")
+        return player_hp, gold
+
+#who is winning
+"""Displays the current HP of the player and monster."""
+def display_fight_statistics(player_hp, monster_hp):
+    print(f"Your HP: {player_hp}, Monster HP: {monster_hp}")
+
+#what to do in fight?
+"""Gets the user's choice during a fight.
+
+called with get_user_fight_options, should be called from gamefunctions"""
+    
+def get_user_fight_options():
+    while True:
+        print("1) Continue Fighting")
+        print("2) Run Away")
+        choice = input("Enter your choice (1 or 2): ")
+        if choice in ("1", "2"):
+            return choice
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
+#nap time code
+"""sleep action restores health after removing 5 gold from your inventory
+called with handle_sleep"""
+
+def handle_sleep(player_hp, gold):
+    if gold >= 5:
+        player_hp = 30
+        gold -= 5
+        print("You slept and restored your HP.")
+        return player_hp, gold
+    else:
+        print("You don't have enough gold to sleep.")
+        return player_hp, gold
+
+#purchasing from the shop
+"""this is to be used with a shop code to purchase items within the game
+called on with purchase_item(item_price, starting_money, quantity)"""
 
 def purchase_item(item_price, starting_money, quantity):
     total_purchase = (item_price * quantity)
@@ -33,46 +132,11 @@ def purchase_item(item_price, starting_money, quantity):
         leftover_money = starting_money - (item_price * quantity)
         starting_money
         return(quantity, leftover_money)
-        
 
-
-
-
-#monster code.
-"""monster generator, asigns health, power level, mulitiplies health by the power level, and picks one of three scenerios where you are interacting with the monster.
-
- new_random_monster()
- is the function call, seems to be working great."""
-
-def new_random_monster():
-    monsters = ["demon", "ghost", "goblin", "creeper"]
-    health = random.randint(0,185)
-    money = random.randint(0,185)
-    power = random.randint(1,5)
-    sel_health = health * power
-    sel_monster = random.choice(monsters)
-    #scenarios:
-    scenario = [(f'You come across a {sel_monster} eating a corpse'), (f"A {sel_monster} jumps out from beind a crate"), (f'After entering a cave a {sel_monster} attacks you from a dark corner')]
-    sel_scenario = random.choice(scenario)
-                                                                   
-    return(sel_health, money, sel_monster, sel_scenario)
-
-
-
-
-"""takes name as arg returns "hello name!" centered.
-
-name = input()
-print_welcome(name)"""
-
-def print_welcome(name):
-    text = (f"Hello {name}")
-    centered_text = text.center(15)
-    print(centered_text)
-
-"""shop menu, prints nicely formatted shop menu. Items can be changed as the function is called.
-
-print_shop_menu('milk', 14.99, 'pie', 18.48)"""
+#visual menu I made for the shop
+"""this is to print a small menu for the shop, currently
+it holds only two items in a small box of text
+called on with print_shop_ menu(item1, x: float, item2, y: float):"""
 
 def print_shop_menu(item1, x: float, item2, y: float):
     print('/' + '-' * 22 + '\\' )
@@ -80,21 +144,5 @@ def print_shop_menu(item1, x: float, item2, y: float):
     print(f"|{item2:<15}{y:>7}|",)
     print('\\' + '-' * 22 + '/' )
 
+"""inventory and shop stuff will be held below"""
 
-
-
-
-""" testing functions held in this file, but only if called from this file
-"""
-def test_functions():
-        #fuction for shop =D
-    print_shop_menu('milk', 14.99, 'pie', 18.48)
-    #welcome "name"
-    name = input()
-    print_welcome(name)
-    #fuction for monster
-    new_random_monster()
-    #purchase item
-    purchase_item ((random.random()), random.randint(1,6), random.randint(0,4))
-if __name__ == "__main__":
-    test_functions()
--- pythongame.py
+++ pythongame.py
@@ -1,102 +1,34 @@
+import gamefunctions
 import random
-
-def display_fight_statistics(player_hp, monster_hp):
-    """Displays the current HP of the player and monster."""
-    print(f"Your HP: {player_hp}, Monster HP: {monster_hp}")
-
-def get_user_fight_options():
-    """Gets the user's choice during a fight."""
-    while True:
-        print("1) Continue Fighting")
-        print("2) Run Away")
-        choice = input("Enter your choice (1 or 2): ")
-        if choice in ("1", "2"):
-            return choice
-        else:
-            print("Invalid choice. Please enter 1 or 2.")
-
-def handle_fight(player_hp, gold):
-    """Handles the monster fight."""
-    monster_hp = 15
-    monster_attack = 5
-    monster_type = "Goblin"
-    print(f"A wild {monster_type} appears!")
-
-    while player_hp > 0 and monster_hp > 0:
-        display_fight_statistics(player_hp, monster_hp)
-        choice = get_user_fight_options()
-
-        if choice == "1":
-            player_attack = random.randint(3, 8)
-            monster_hp -= player_attack
-            print(f"You attack the {monster_type} for {player_attack} damage.")
-
-            if monster_hp > 0:
-                monster_damage = random.randint(1, monster_attack)
-                player_hp -= monster_damage
-                print(f"The {monster_type} attacks you for {monster_damage} damage.")
-
-        elif choice == "2":
-            print("You run away!")
-            return player_hp, gold
-
-    if player_hp <= 0:
-        print("You were defeated!")
-        return 0, gold
-    else:
-        print(f"You defeated the {monster_type}!")
-        gold += 10
-        print("You found 10 gold.")
-        return player_hp, gold
-
-def handle_sleep(player_hp, gold):
-    """Handles the sleep action."""
-    if gold >= 5:
-        player_hp = 30
-        gold -= 5
-        print("You slept and restored your HP.")
-        return player_hp, gold
-    else:
-        print("You don't have enough gold to sleep.")
-        return player_hp, gold
-
-def get_user_town_choice():
-    """Gets the user's choice in town and validates it."""
-    while True:
-        print("\nYou are in town.")
-        print(f"Current HP: {player_hp}, Current Gold: {gold}")
-        print("What would you like to do?")
-        print("1) Leave town (Fight Monster)")
-        print("2) Sleep (Restore HP for 5 Gold)")
-        print("3) Quit")
-
-        choice = input("Enter your choice (1, 2, or 3): ")
-        if choice in ("1", "2", "3"):
-            return choice
-        else:
-            print("Invalid choice. Please enter 1, 2, or 3.")
 
 def game_loop():
     """Main game loop."""
     global player_hp, gold #player_hp and gold are needed to be global to be modified in other functions.
     player_hp = 30
     gold = 10
+    inventory = []
 
     while True:
-        choice = get_user_town_choice()
+        choice = gamefunctions.get_user_town_choice(player_hp, gold)
 
         if choice == "1":
             if gold >= 0:
-                player_hp, gold = handle_fight(player_hp, gold)
+                player_hp, gold = gamefunctions.handle_fight(player_hp, gold)
                 if player_hp <= 0:
                     player_hp = 30 #reset hp on death.
             else:
                 print("You are too weak to fight, you run away.")
 
         elif choice == "2":
-            player_hp, gold = handle_sleep(player_hp, gold)
+            player_hp, gold = gamefunctions.handle_sleep(player_hp, gold)
 
         elif choice == "3":
+            pass
+            #gamefunctions.display_inventory(inventory)
+        elif choice == "4":
+            pass
+            #shop
+        elif choice == "5":
             print("Quitting the game. Goodbye!")
             break
 
